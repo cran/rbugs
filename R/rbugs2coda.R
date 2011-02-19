90466775305a5724c2500d2cfb1961e6d2a42e9b
Marcos Prates <marcosop@ime.unicamp.br>
2011-02-19 00:00:00 +0000

    version 0.5-2

--- R/rbugs2coda.R
+++ R/rbugs2coda.R
@@ -1,5 +1,5 @@
-## Print the statistics summary of the chains
-rbugs2coda <- function(model)
+## Convert a rbugs object to a coda object
+rbugs2coda <- function(model, burnin = NULL, thin = NULL)
 {
     
     if( class(model) != "rbugs") stop("\nThe model is not a rbugs obejct.\n")
@@ -10,7 +10,17 @@ rbugs2coda <- function(model)
     obj <- list()
     for (i in 1:model$n.chain)
       obj <- c(obj,list(as.mcmc(model[[chain.name[i]]])))
-      
+    
+    if(length(burnin) != 0){
+      for (i in 1:model$n.chain)
+       obj[[i]] <- mcmc(obj[[i]], start = burnin, end = nrow(obj[[i]])) 
+    } 
+    
+    if(length(thin) != 0){
+      for (i in 1:model$n.chain)
+       obj[[i]] <- mcmc(obj[[i]], start = 1, end = nrow(obj[[i]]), thin = thin) 
+    }
+    
     obj <- as.mcmc.list(obj)
     
     obj
